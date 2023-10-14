74380a20a01b9f714c525004795237c5ed5d2d2b
macbookpro <muhlis.asri@gmail.com>
2023-10-14 22:20:39 +0800

    change align to justify

--- studentproject/studpro.py
+++ studentproject/studpro.py
@@ -9,11 +9,11 @@ for file in files:
     title = Path(file).stem.title()
     pdf.add_page()
     pdf.set_font(family="Times", style="B", size=20)
-    pdf.cell(w=50, h=7, txt=title)
+    pdf.cell(w=150, h=7, txt=title)
     pdf.ln(10)
     with open(file, "r") as content:
         content = content.read()
         pdf.set_font(family="Times", size=12)
-        pdf.write(h=6, txt=content)
+        pdf.multi_cell(w=180, h=6, txt=content, align="J")
 
 pdf.output("studpro.pdf")
